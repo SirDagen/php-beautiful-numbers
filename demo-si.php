c48a085beb26414b8f3b1afef4838c8d5450df17
SirDagen <8721141+SirDagen@users.noreply.github.com>
2019-05-09 00:23:46 +0200

    Update demo-si.php

--- demo-si.php
+++ demo-si.php
@@ -2,7 +2,6 @@
 
 require('PHPzformat.php'); 
 
-
 #
 # --- Demo sinum: sinum($value, $unit, ['acc'=>3, 'bin'=>false])
 #
@@ -12,23 +11,23 @@ $zformat = new PHPzformat\zformat([',', '.']); // German number format
 
 echo "<br/><p><b>Das Nummerformat ist Deutsch:</b><br/></p>"; 
 
-$q=mt_rand()/mt_getrandmax(); $u='s'; 
-echo "<p>".$zformat->sinum($q, $u)." &nbsp;(= {$q} {$u})</p>"; 
+$val=mt_rand()/mt_getrandmax(); $u='s'; 
+echo "<p>".$zformat->sinum($val, $u)." &nbsp;(= {$val} {$u})</p>"; 
 
-$q=mt_rand()/mt_getrandmax()/1000; $u='s'; 
-echo "<p>".$zformat->sinum($q, $u)." &nbsp;(= {$q} {$u})</p>";
+$val=mt_rand()/mt_getrandmax()/1000; $u='s'; 
+echo "<p>".$zformat->sinum($val, $u)." &nbsp;(= {$val} {$u})</p>";
 
-$q=mt_rand()/mt_getrandmax()/10000000; $u='s'; 
-echo "<p>".$zformat->sinum($q, $u)." &nbsp;(= {$q} {$u})</p>";
+$val=mt_rand()/mt_getrandmax()/10000000; $u='s'; 
+echo "<p>".$zformat->sinum($val, $u)." &nbsp;(= {$val} {$u})</p>";
 
-$q=mt_rand()/mt_getrandmax()*2000; $u='g'; 
-echo "<p>".$zformat->sinum($q, $u)." &nbsp;(= {$q} {$u})</p>";
+$val=mt_rand()/mt_getrandmax()*2000; $u='g'; 
+echo "<p>".$zformat->sinum($val, $u)." &nbsp;(= {$val} {$u})</p>";
 
-$q=mt_rand(5, 1999999); $u='B'; 
-echo "<p>".$zformat->sinum($q, $u, ['bin'=>true])." &nbsp;(= {$q} {$u})</p>";
+$val=mt_rand(5, 1999999); $u='B'; 
+echo "<p>".$zformat->sinum($val, $u, ['bin'=>true])." &nbsp;(= {$val} {$u})</p>";
 
-$q=mt_rand()/mt_getrandmax()*9000; $u='m'; 
-echo "<p>".$zformat->sinum($q, $u, ['acc'=>4])." &nbsp;(= {$q} {$u})</p>"; // accuracy is set to 4 decimal digits
+$val=mt_rand()/mt_getrandmax()*9000; $u='m'; 
+echo "<p>".$zformat->sinum($val, $u, ['acc'=>4])." &nbsp;(= {$val} {$u})</p>"; // accuracy is set to 4 decimal digits
 
  
 
@@ -37,23 +36,23 @@ $zformat = new PHPzformat\zformat(['.', ',']); // English number format
 
 echo "<br/><p><b>The number format is set to English:</b><br/></p>"; 
 
-$q=mt_rand()/mt_getrandmax(); $u='s'; 
-echo "<p>".$zformat->sinum($q, $u)." &nbsp;(= {$q} {$u})</p>"; 
+$val=mt_rand()/mt_getrandmax(); $u='s'; 
+echo "<p>".$zformat->sinum($val, $u)." &nbsp;(= {$val} {$u})</p>"; 
 
-$q=mt_rand()/mt_getrandmax()/1000; $u='s'; 
-echo "<p>".$zformat->sinum($q, $u)." &nbsp;(= {$q} {$u})</p>";
+$val=mt_rand()/mt_getrandmax()/1000; $u='s'; 
+echo "<p>".$zformat->sinum($val, $u)." &nbsp;(= {$val} {$u})</p>";
 
-$q=mt_rand()/mt_getrandmax()/10000000; $u='s'; 
-echo "<p>".$zformat->sinum($q, $u)." &nbsp;(= {$q} {$u})</p>";
+$val=mt_rand()/mt_getrandmax()/10000000; $u='s'; 
+echo "<p>".$zformat->sinum($val, $u)." &nbsp;(= {$val} {$u})</p>";
 
-$q=mt_rand()/mt_getrandmax()*2000; $u='g'; 
-echo "<p>".$zformat->sinum($q, $u)." &nbsp;(= {$q} {$u})</p>";
+$val=mt_rand()/mt_getrandmax()*2000; $u='g'; 
+echo "<p>".$zformat->sinum($val, $u)." &nbsp;(= {$val} {$u})</p>";
 
-$q=mt_rand(5, 1999999); $u='B'; 
-echo "<p>".$zformat->sinum($q, $u, ['bin'=>true])." &nbsp;(= {$q} {$u})</p>";
+$val=mt_rand(5, 1999999); $u='B'; 
+echo "<p>".$zformat->sinum($val, $u, ['bin'=>true])." &nbsp;(= {$val} {$u})</p>";
  
-$q=mt_rand()/mt_getrandmax()*9000; $u='m'; 
-echo "<p>".$zformat->sinum($q, $u, ['acc'=>4])." &nbsp;(= {$q} {$u})</p>"; // accuracy is set to 4 decimal digits
+$val=mt_rand()/mt_getrandmax()*9000; $u='m'; 
+echo "<p>".$zformat->sinum($val, $u, ['acc'=>4])." &nbsp;(= {$val} {$u})</p>"; // accuracy is set to 4 decimal digits
 
 
 
