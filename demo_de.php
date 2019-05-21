9ea8b6772ea30d6c3989d67369651ec0b61ef189
Gordon <8721141+SirDagen@users.noreply.github.com>
2019-05-21 16:43:37 +0200

    Update demo_de.php

--- demo_de.php
+++ demo_de.php
@@ -46,13 +46,15 @@ echo "<p>".$bn->sinum($val)." &nbsp;(= {$val})</p>";
 $val=round(mt_rand()/mt_getrandmax()*80000000000+1000000000);  
 echo "<p>".$bn->sinum($val)." &nbsp;(= {$val})</p>"; 
 
-echo "<p><br/><b>Sie können auch einen statistischen Fehler angeben (anstelle der Genauigkeit):</b></p>";
+echo "<p><br/><b>Sie können auch einen statistischen Fehler angeben (anstelle der Genauigkeit)</b></p>";
+
+$bnW = new bnformat\bnformat( ['lang'=>'de-WI'] ); 
 
 $val=mt_rand()/mt_getrandmax()*2000; $u='g'; $err=$val/900; 
-echo "<p>".$bn->sinum($val, $u, ['err'=>$err])." &nbsp;(= {$val} ± {$err} {$u})</p>";
+echo "<p>".$bnW->sinum($val, $u, ['err'=>$err])." &nbsp;(= {$val} ± {$err} {$u})</p>";
 
-$val=mt_rand()/mt_getrandmax()/10000000; $u='s'; $err=$val/1200; 
-echo "<p>".$bn->sinum($val, $u, ['err'=>$err])." &nbsp;(= {$val} ± {$err} {$u})</p>";
+$val=mt_rand()/mt_getrandmax()/10000000; $u='s'; $err=$val/1200;  
+echo "<p>".$bnW->sinum($val, $u, ['err'=>$err])." &nbsp;(= {$val} ± {$err} {$u})</p>";
 
 
 // tnum()
