d4a0e52e9eb499dec755f319bf4463de8e0f73a6
Gordon <8721141+SirDagen@users.noreply.github.com>
2019-05-23 23:59:12 +0200

    Update demo_de.php

--- demo_de.php
+++ demo_de.php
@@ -10,12 +10,9 @@ require('bnformat-class.php');
 // Neue Instanz anlegen - Deutsches Nummberformat
 $bn = new bnformat\bnformat( ['lang'=>'de'] ); 
 
-echo "<br/><h1>php-beautiful-numbers <b>(Format = ".$bn->langname().")</b><br/></h1>"; 
-
-
 // sinum()
 
-echo "<br/><h3><pre>A. sinum() &ndash; Gibt Zahlen im einfach zu lesenden SI-Format aus</pre></h3>";
+echo "<br/><h3><pre>1. sinum() &ndash; Gibt Zahlen im einfach zu lesenden SI-Format aus</pre></h3>";
 
 $val=mt_rand()/mt_getrandmax()/5; $u='s'; 
 echo "<p>".$bn->sinum($val, $u)." &nbsp;(= {$val} {$u})</p>"; 
@@ -33,7 +30,7 @@ $val=mt_rand(5, 1999999); $u='B';
 echo "<p>".$bn->sinum($val, $u, ['bin'=>true])." &nbsp;(= {$val} {$u})</p>";
 
 $val=mt_rand()/mt_getrandmax()*9000; $u='m'; 
-echo "<p>".$bn->sinum($val, $u, ['acc'=>2])." &nbsp;(= {$val} {$u})</p>"; // Genauigkeit = 2 Stellen
+echo "<p>".$bn->sinum($val, $u, ['acc'=>2])." &nbsp;(= {$val} {$u}) [Genauigkeit: 2 Stellen]</p>"; // Genauigkeit = 2 Stellen
 
 echo "<p><br/><b>kilo, Mega, Giga, Tera, ...</b></p>";
 
@@ -46,20 +43,10 @@ echo "<p>".$bn->sinum($val)." &nbsp;(= {$val})</p>";
 $val=round(mt_rand()/mt_getrandmax()*80000000000+1000000000);  
 echo "<p>".$bn->sinum($val)." &nbsp;(= {$val})</p>"; 
 
-echo "<p><br/><b>Sie können auch einen statistischen Fehler angeben (anstelle der Genauigkeit)</b></p>";
-
-$bnW = new bnformat\bnformat( ['lang'=>'de-WI'] ); 
-
-$val=mt_rand()/mt_getrandmax()*2; $u='g'; $err=$val/900; 
-echo "<p>".$bnW->sinum($val, $u, ['err'=>$err])." &nbsp;(= {$val} ± {$err} {$u})</p>";
-
-$val=mt_rand()/mt_getrandmax()/10000000; $u='s'; $err=$val/1200;  
-echo "<p>".$bnW->sinum($val, $u, ['err'=>$err])." &nbsp;(= {$val} ± {$err} {$u})</p>";
-
 
 // tnum()
 
-echo "<br/><h3><pre>B. tnum() &ndash; Gibt Zahlen im Fließtext aus (0..12 werden ausgeschrieben) - text number</pre></h3>";
+echo "<br/><h3><pre>2.1. tnum() &ndash; Gibt Zahlen im Fließtext aus (0..12 werden ausgeschrieben) - text number</pre></h3>";
 
 // Zahlen 0..12 werden ausgeschrieben, alle anderen als Zahlen ausgegeben (wie es in Publikationen üblich ist)
 echo "<p>"; 
@@ -100,7 +87,7 @@ echo "</p>";
 
 // tsyn() - zusätzliche Nutzung von 
 
-echo "<br/><h3><pre>C. tsyn() &ndash; Unterscheidet zwischen Singular und Plural - text syntax</pre></h3>";
+echo "<br/><h3><pre>2.2. tsyn() &ndash; Unterscheidet zwischen Singular und Plural - text syntax</pre></h3>";
 
 echo "<p>"; 
 for ($i=0;$i<3;$i++) { 
@@ -108,6 +95,22 @@ for ($i=0;$i<3;$i++) {
     echo "Es ".$bn->tsyn($val, ['stehen', 'steht'])." ".$bn->tnum($val, ['Bäume', 'ein Baum'])." auf dem Marktplatz.<br/>"; 
 }
 echo "</p>"; 
+
+
+// sinum() - statistische Nutzung
+
+echo "<br/><h3><pre>3. sinum() &ndash; Statistische Nutzung</pre></h3>";
+
+$bnW = new bnformat\bnformat( ['lang'=>'de-WI'] ); 
+
+$val=mt_rand()/mt_getrandmax()*2; $u='g'; $err=$val/900; 
+echo "<p>".$bnW->sinum($val, $u, ['err'=>$err])." &nbsp;(= {$val} ± {$err} {$u})</p>";
+
+$val=mt_rand()/mt_getrandmax()/10000000; $u='s'; $err=$val/1200;  
+echo "<p>".$bnW->sinum($val, $u, ['err'=>$err])." &nbsp;(= {$val} ± {$err} {$u})</p>";
+
+$val=mt_rand()/mt_getrandmax()/100; $u='m'; $err=$val/800;  
+echo "<p>".$bnW->sinum($val, $u, ['err'=>$err])." &nbsp;(= {$val} ± {$err} {$u})</p>";
 
 
 
